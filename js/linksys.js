83a444e61a0a4713ae44529187219a865e50cc47
suddingpudding <ssu9175@gmail.com>
2018-03-08 20:54:26 +0900

    html uplode

---
+++ js/linksys.js
@@ -0,0 +1,16 @@
+// link system js
+
+$(function(){/////////////////////////////
+    
+    //로고 클릭시 메인페이지로 이동
+    $(".logo").click(function(){
+        location.href = "index.html";
+    });
+    
+    
+    
+    
+    
+    
+    
+});//JQB////////////////////////////////
